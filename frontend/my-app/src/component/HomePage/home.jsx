3fa36eec1d9682715e738d234e0d7996a348528f
Hitesh11112 <hiteshpatwal2004@gmail.com>
2026-08-23 23:12:19 +0530

    added Atcoder in items frontend

--- frontend/my-app/src/component/HomePage/home.jsx
+++ frontend/my-app/src/component/HomePage/home.jsx
@@ -3,7 +3,7 @@ import { useState, useRef } from "react";
 const menu = [
   {
     label: "Coding Platforms",
-    items: ["LeetCode", "Codeforces", "HackerRank", "CodeChef","CSES"],
+    items: ["LeetCode", "Codeforces", "HackerRank", "CodeChef","CSES","Atcoder"],
   },
   {
     label: "Online Resources",
